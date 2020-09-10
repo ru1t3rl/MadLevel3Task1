a55e68c4fe0be8dbe6099363e650d04a8823cf56
ru1t3rl <luca.ruiters@hva.nl>
2020-09-10 18:56:35 +0100

    Added the navigation between the start screen and the rating view

--- app/src/main/java/tech/tucano/madlevel3task1/StartFragment.kt
+++ app/src/main/java/tech/tucano/madlevel3task1/StartFragment.kt
@@ -7,21 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import tech.tucano.madlevel3task1.databinding.FragmentStartBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class StartFragment : Fragment() {
+    private  lateinit var binding: FragmentStartBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStartRating.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_startFragment_to_ratingFragment
+            )
+        }
     }
 }
